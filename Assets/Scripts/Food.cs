26bbefe8525c932d928ce43775297640501c857d
Saltinvolved <alina.grohe@live.de>
2023-01-18 18:27:48 +0100

    Pups Sound bei Item Collect. Scrißt ist auf Food item .wäre es auf Player besser ?

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class Food : MonoBehaviour
 {
     public GameObject AstroBoi;
+    public ParticleSystem Farts;
+    public AudioSource fartSound;
+    
 
     //Fruits get destroyed when Astroboi touches them
     private void OnTriggerEnter (Collider other)
     {
-        Debug.Log (other.name + " just hit " + gameObject.name);
+        Debug.Log (other.name + " just hit "+ gameObject.name);
         if(other.name == AstroBoi.name)
         {
-            //destroy fruit after 0 seconds
-            Destroy (gameObject,0);
+            
+        EmitParticles(); 
+        fartSound.Play();
+        Destroy (gameObject,0);
+           
+           
+           
+
+            
         }
     }
+
+    void EmitParticles()
+    {
+        Farts.Emit(50);
+    }
+
+ 
 }
