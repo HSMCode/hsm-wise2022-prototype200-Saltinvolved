6adccb73b267c2f4db75b11a062fb8d5ed9f2d5c
Saltinvolved <alina.grohe@live.de>
2023-01-18 22:49:06 +0100

    Item Collect = speed Boost

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public float time = 0.0f;
-    public float moveSpeed;
+    public float normalSpeed = 5f;
     public Rigidbody _playerRb;
     private bool isMoving = false;
     private bool IsJumpPressed = false;
     private Vector3 moveDirection;
 
+    // Variables for Item Collect
+   
+    public ParticleSystem Farts;
+    public AudioSource fartSound;
+    public GameObject astroBoi;
+
+    // Boost Variablen 
+    private float speed;
+    private float speedBoost = 8.5f;
+    private float speedBoostDuration = 2f;
+
+    private bool boostActive;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
+        boostActive = false; 
+        if (boostActive)
+        {
+            speed = speedBoost;
+        }
+        else 
+        {
+            speed = normalSpeed;
+        }
     }
 
     // Update is called once per frame
@@ -58,6 +82,45 @@ public class CharacterMovement : MonoBehaviour
 
     void Move()
     {
-        _playerRb.velocity = new Vector3(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        
+        _playerRb.velocity = new Vector3(moveDirection.x * speed, moveDirection.y * speed);
     }
+
+
+// versuch Boost 
+// Corutine 
+
+public void ActivateSpeedBoost()
+{
+    StartCoroutine(SpeedBoostCooldown());
 }
+
+IEnumerator SpeedBoostCooldown()
+{
+    speed = speedBoost;
+    yield return new WaitForSeconds(speedBoostDuration);
+    speed = normalSpeed;
+
+}
+
+// Food Collect auf diesem script ? 
+
+private void OnTriggerEnter(Collider other)
+ {
+    Debug.Log (other.name + " just hit "+ gameObject.name);
+    if (other.CompareTag("Food"))
+    {
+        EmitParticles(); 
+        fartSound.Play();
+        //Destroy (other.CompareTag("Food"),0);
+        ActivateSpeedBoost();
+    }
+ }
+       
+    void EmitParticles()
+    {
+        Farts.Emit(50);
+    }
+
+}
+
